00c68e0ff8da4d23f8d3288316ee50c1f5cb0a16
Brian Kiesel <app_academy_student_bk@protonmail.com>
2022-09-24 15:43:41 -0700

    passed ALL W9D1P3L

--- practice-for-week-09-window-object-and-debugger-long-practice/assets/js/manipulate.js
+++ practice-for-week-09-window-object-and-debugger-long-practice/assets/js/manipulate.js
@@ -1,18 +1,22 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    document.title = "Brian's Portfolio";
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    document.body.children[0].children[0].innerText = "I am Brian Kiesel";
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+    document.body.children[1].children[1].innerText = "Hey Now Brown Cow!!";
+    document.body.children[1].children[2].innerText = "Do you like pickled armadillos feet?!";
 }
